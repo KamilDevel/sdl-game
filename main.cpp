4c049f061b93ccb6d099a14c63492910b388b878
Kamil Badura <kamil.devel@gmail.com>
2020-06-20 11:37:21 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * Created by Kamil Badura on 20/06/2020
+ * Copyright (c) 2020 Kamil Badura <kamil.devel@gmail.com>
+ * All rights reserved.
+ *
+ * main.cpp
+ */
+
+#include "main.h"
+
+/**
+ * Main program
+ */
+int main(void)
+{
+
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+/**
+ * Created by Kamil Badura on 20/06/2020
+ * Copyright (c) 2020 Kamil Badura <kamil.devel@gmail.com>
+ * All rights reserved.
+ *
+ * main.h
+ */
+
+#pragma once
